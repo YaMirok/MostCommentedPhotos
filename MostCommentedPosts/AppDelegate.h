4c7f8082e0e5e970aa8fc9871f4921c2f2cfa60b
Damir Sitdikov <YaMirok@MacBook-Pro-Damir.local>
2015-08-16 11:52:20 +0500

    Initial Commit

---
+++ MostCommentedPosts/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MostCommentedPosts
+//
+//  Created by Damir Sitdikov on 16.08.15.
+//  Copyright (c) 2015 Damir Sitdikov. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ MostCommentedPosts/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MostCommentedPosts
+//
+//  Created by Damir Sitdikov on 16.08.15.
+//  Copyright (c) 2015 Damir Sitdikov. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
